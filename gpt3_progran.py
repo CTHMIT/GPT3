3007f2e1ca803e9114e0fe1ed965e7e5731deaaa
Mike_tw <107465888+CTHMIT@users.noreply.github.com>
2022-07-22 12:04:06 +0800

    Create gpt3_progran.py
    Extract, Reconstruct & Similarity

---
+++ gpt3_progran.py
@@ -0,0 +1,49 @@
+import os
+import openai
+
+def gpt3_Extact(input, temper=0, eng="text-davinci-002"):
+  step_name = "Extract keywords"
+  openai.api_key = key
+  response = openai.Completion.create(
+                    engine = eng,
+                    prompt = f"{step_name} from : {input}",
+                    temperature = temper,
+                    top_p = 1.0,
+                    max_tokens = 128,
+                    )
+  sentance = replace_progran(response.choices[0].text)
+  prompt = response.usage.prompt_tokens
+  total = response.usage.total_tokens
+  similar = gpt3_similarity(sentance)
+  return sentance, prompt, total, step_name, input, similar
+    
+def gpt3_reconstruct(input, temper=0.5, eng="text-davinci-002"):
+  source = input[0]
+  step_name = "Reconstruction sentance"
+  openai.api_key = key
+  response = openai.Completion.create(
+                    engine = eng,
+                    prompt = f"{step_name} from these keywords : {source}",
+                    temperature = temper,
+                    top_p = 1.0,
+                    max_tokens = 128,
+                    )
+  sentance = replace_progran(response.choices[0].text)
+  prompt = response.usage.prompt_tokens
+  total = response.usage.total_tokens
+  similar = gpt3_similarity(sentance)
+  return sentance, prompt, total, step_name, source, similar
+
+def gpt3_similarity(input, eng="text-davinci-002"):
+  step_name = "similarity score and semantic similarity score"
+  similarity_sentance = f"{original_sentance} and {input}"
+  openai.api_key = key
+  response = openai.Completion.create(
+                    engine = eng,
+                    prompt = f"Give a {step_name} between: {similarity_sentance}",
+                    temperature = 0,
+                    top_p = 1.0,
+                    max_tokens = 128,
+                    )
+  output = replace_progran(response.choices[0].text)
+  return output
