1693e60ef6731b4b5ed40c24ca8673abf49e0054
Mike_tw <107465888+CTHMIT@users.noreply.github.com>
2022-07-22 12:56:53 +0800

    Update gpt3_progran.py

--- gpt3_progran.py
+++ gpt3_progran.py
@@ -1,9 +1,10 @@
 import os
 import openai
 
+openai.api_key  = "You're API Ket"
+
 def gpt3_Extact(input, temper=0, eng="text-davinci-002"):
   step_name = "Extract keywords"
-  openai.api_key = key
   response = openai.Completion.create(
                     engine = eng,
                     prompt = f"{step_name} from : {input}",
@@ -20,7 +21,6 @@ def gpt3_Extact(input, temper=0, eng="text-davinci-002"):
 def gpt3_reconstruct(input, temper=0.5, eng="text-davinci-002"):
   source = input[0]
   step_name = "Reconstruction sentance"
-  openai.api_key = key
   response = openai.Completion.create(
                     engine = eng,
                     prompt = f"{step_name} from these keywords : {source}",
@@ -37,7 +37,6 @@ def gpt3_reconstruct(input, temper=0.5, eng="text-davinci-002"):
 def gpt3_similarity(input, eng="text-davinci-002"):
   step_name = "similarity score and semantic similarity score"
   similarity_sentance = f"{original_sentance} and {input}"
-  openai.api_key = key
   response = openai.Completion.create(
                     engine = eng,
                     prompt = f"Give a {step_name} between: {similarity_sentance}",
